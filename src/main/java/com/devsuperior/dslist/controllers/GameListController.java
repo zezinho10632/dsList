5a17fb38c64feab473b7b4fe70f62a930d8347c4
José <jn10632@gmail.com>
2025-02-11 01:14:28 -0300

    Homogolaçao concluida

--- src/main/java/com/devsuperior/dslist/controllers/GameListController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -1,17 +1,13 @@
 package com.devsuperior.dslist.controllers;
 
-import com.devsuperior.dslist.dto.GameDTO;
-import com.devsuperior.dslist.dto.GameListDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.dto.GameListDTO;
 import com.devsuperior.dslist.servicies.GameListService;
 import com.devsuperior.dslist.servicies.GameService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
 
 @RestController
 @RequestMapping(value = "/lists")
@@ -20,25 +16,19 @@ public class GameListController {
     private final GameListService gameListService;
     private final GameService gameService;
 
-    public GameListController(
-            GameListService gameListService,
-            GameService gameService
-    ) {
+    public GameListController(GameListService gameListService, GameService gameService) {
         this.gameListService = gameListService;
         this.gameService = gameService;
     }
 
-
     @GetMapping
     public List<GameListDTO> findAll() {
-        List<GameListDTO> result = gameListService.findAll();
-        return result;
-
+        return gameListService.findAll();
     }
-    @GetMapping(value = "/{listId}/games")
-    public List<GameMinDTO> findByList(@PathVariable Long listId) {
-        List<GameMinDTO> result = gameService.findByList(listId);
-        return result;
 
+    @GetMapping(value = "/{listId}/games")
+    public ResponseEntity<List<GameMinDTO>> findByList(@PathVariable Long listId) {
+        List<GameMinDTO> result = gameService.findByList(listId);
+        return ResponseEntity.ok(result);
     }
 }
--- src/main/java/com/devsuperior/dslist/servicies/GameListService.java
+++ src/main/java/com/devsuperior/dslist/servicies/GameListService.java
@@ -1,34 +1,22 @@
 package com.devsuperior.dslist.servicies;
 
-import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameListDTO;
-import com.devsuperior.dslist.dto.GameMinDTO;
-import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.entities.GameList;
 import com.devsuperior.dslist.repositories.GameListRepository;
-import com.devsuperior.dslist.repositories.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class GameListService {
 
+    @Autowired
+    private GameListRepository gameListRepository;
 
-    private final GameListRepository gameListRepository;
-
-    public GameListService(GameListRepository gameListRepository) {
-        this.gameListRepository = gameListRepository;
-    }
-
-
-    @Transactional(readOnly = true)
     public List<GameListDTO> findAll() {
-        List<GameList> result = gameListRepository.findAll();
-        return result.stream().map(GameListDTO::new).toList();
+        List<GameList> lists = gameListRepository.findAll();
+        return lists.stream().map(GameListDTO::new).collect(Collectors.toList());
     }
-
-
 }
